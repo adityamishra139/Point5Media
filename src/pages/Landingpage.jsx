daac52dd5b029cdc853ee615df2d6ff92ed4b8fd
Ananya Singh <ananyasingh3937@gmail.com>
2024-10-19 14:50:09 +0530

    pink

--- src/pages/Landingpage.jsx
+++ src/pages/Landingpage.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import { LampDemo } from "../components/LampEffect"; // Named import
 
+
 const Landingpage = () => {
+  
   return (
+    
     <div>
       {/* Navbar */}
       <Navbar />
       
       {/* Lamp Effect */}
       <LampDemo />
-
       {/* Footer */}
     </div>
   );
